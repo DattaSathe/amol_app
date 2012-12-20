0a3039922a9975302fb0365be506f05d06e51459
Amol <amol@amol-VGN-FE770G.(none)>
2012-12-21 03:04:02 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmolApp::Application.config.secret_token = 'a09265827a9cf1c308a11046e20f581a68d2ae9f7ded5419d0a3ff666ef8e68dc3ba58e069f07106decf354b11a3391844d310177f76896728d8e0170cc23272'
